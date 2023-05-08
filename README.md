cef61484870dbd389569770150a068a43d6ecb45
melckzedeck <melckzedeck063@gmail.com>
2023-05-08 07:58:27 +0300

    clinton_E_solution initial commit with user registration functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+melckzedeck james @melckzedeck063
+# clinton_e_server
+#using node js express and mongoDB
---
+++ server/routes/userRoute.js
@@ -0,0 +1,20 @@
+const express =  require('express');
+
+const router =  express.Router();
+
+const authController  =   require('../controllers/AuthController');
+const  userController =  require('../controllers/userController');
+
+router.post('/signup', authController.signUp);
+router.post('/login', authController.Login);
+
+// router.use(authController.protect);
+
+router.get('/all_users', userController.getAllUsers);
+// router.get('/all_staffs', userController.getAllStaffs);
+router.get('/me', userController.getMe);
+router.get('/user/:id', userController.getUser);
+router.patch('/update_user/:id', userController.updateUser);
+
+
+module.exports =  router;
